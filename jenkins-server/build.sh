4e645ef4aaa4940ca449779eed118df30e8199a7
ISSDU Tony <12228143+issdutony@users.noreply.github.com>
2026-05-04 23:17:42 +0800

    modify files

--- jenkins-server/build.sh
+++ jenkins-server/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+IMAGE_NAME="rd/jenkins:lts-jdk21"
+
 docker builder prune -a -f
-docker build --no-cache --network=host -t rd/jenkins:lts-jdk21 .
+
+docker image rm ${IMAGE_NAME}
+
+docker build --no-cache -t ${IMAGE_NAME} .
+# docker build --no-cache --network=host -t ${IMAGE_NAME} .
--- portainer/readme.md
+++ portainer/readme.md
@@ -7,5 +7,5 @@
 ## Setting
 
 - Environment-related > Environments > local
-    - Name : `rd-lab`
-    - Public IP : `rd-docker`
+    - Name : 可修改成自定的 Environment, e.g, `rd-lab`
+    - Public IP : 可連線的 HostName 或 IP Adddress, e.g, `rd-docker` or `rd-docker.local`
