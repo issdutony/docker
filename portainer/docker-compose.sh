cffe1813faa1eaa58a736ea7449ae4886acfbf73
ISSDU Tony <12228143+issdutony@users.noreply.github.com>
2026-05-04 18:08:44 +0800

    modify files

--- portainer/docker-compose.sh
+++ portainer/docker-compose.sh
@@ -11,8 +11,8 @@ docker run -d \
     -e TZ=Asia/Taipei \
     -v portainer_data:/data \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v /etc/timezone:/etc/timezone:ro \
+    # -v /etc/localtime:/etc/localtime:ro \
+    # -v /etc/timezone:/etc/timezone:ro \
     --network portainer_network \
     -p 8000:8000 \
     -p 9443:9443 \
